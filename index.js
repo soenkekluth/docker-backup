fd0e197baf8fecbe225276913b00aee75b8ccd53
Sönke Kluth <mail@soenkekluth.com>
2016-08-21 00:34:55 +0200

    0.2.0

--- index.js
+++ index.js
@@ -1,130 +1,142 @@
 #!/usr/bin/env node
 
-var program = require('commander');
-var cmd = require('node-cmd');
-var path = require('path');
+'use strict'
 
-program
-  .version('0.0.1');
+const program = require('commander');
+const cmd = require('node-cmd');
+const path = require('path');
+const rekcod = require('rekcod');
 
-program
-  .command('container')
-  .description('run setup commands for all envs')
-  // .option("-s, --setup_mode [mode]", "Which setup mode to use")
-  .action(function() {
-    cmd.get(
-      'docker ps -a',
-      function(data) {
-        console.log(data);
+const inspect = (container) =>
+  new Promise((resolve, reject) =>
+    cmd.get('docker inspect ' + container, data => resolve(JSON.parse(data)[0])));
+
+const commit = (container, imageName) =>
+  new Promise((resolve, reject) =>
+    cmd.get('docker commit -p ' + container + ' ' + imageName, resolve));
+
+
+const backupImage = (imageName, dest) =>
+  new Promise((resolve, reject) =>
+    cmd.get('docker save -o ' + dest + '/' + imageName + '.tar ' + imageName, resolve));
+
+
+const backupRunCommand = (container, dest) => {
+  rekcod(container, (err, run) => {
+    if (err) {
+      Promise.reject(err);
+      return console.error(err);
+    }
+    var sript = '#!/bin/bash\n\n';
+    sript += run[0].command;
+    cmd.get('echo "' + sript + '" > ' + dest + '/' + container + '_run.sh', () => {
+      cmd.run('chmod +x ' + dest + '/' + container + '_run.sh');
+      console.log('\n\nwriting run script to ' + dest + '/' + container + '_run.sh');
+      console.log('at the moment you need to edit the script as it was created rekcod from npm.\nplease make shure you change the --name, remove the -h option and change the image name to ' + container + '_image');
+    });
+    Promise.resolve(sript);
+  });
+};
+
+
+const backupVolumes = (container, volumes, dest) => {
+  if (!volumes) {
+    return Promise.resolve();
+  }
+
+  return new Promise((resolve, reject) => {
+    var command = 'docker run --rm --volumes-from ' + container + ' -v ' + dest + ':/backup alpine';
+
+    volumes.forEach(function(volume) {
+      if (volume.Name) {
+        command += ' tar cvf /backup/' + container + '_volume_' + volume.Name + '.tar ' + volume.Destination;
       }
-    );
+    });
+
+    cmd.get(command, resolve);
   });
 
-program
-  .command('images')
-  .action(function() {
-    cmd.get(
-      'docker images',
-      function(images) {
-        console.log(images);
-      }
-    );
-  });
+}
 
-program
-  .command('volumes')
-  .action(function() {
-    cmd.get(
-      'docker volume ls',
-      function(data) {
-        console.log(data);
-      }
-    );
-  });
+
+// program
+//   .version('0.0.1');
+
+// program
+//   .command('container')
+//   .description('run setup commands for all envs')
+//   .option('-a, --all', 'show all')
+//   .action(options => {
+//     const opt = options.all ? ' -a' : '';
+//     cmd.get('docker ps' + opt, console.log);
+//   });
+
+
+// program
+//   .command('images')
+//   .option('-a, --all', 'show all')
+//   .action(options => {
+//     const opt = options.all ? ' -a' : '';
+//     cmd.get('docker images' + opt, console.log);
+//   });
+
+// program
+//   .command('volumes')
+//   .action(cmd.get('docker volume ls', console.log));
 
 program
   .command('backup <container> [dest]')
-  .action(function(container, dest) {
+  .action((container, dest) => {
 
     dest = dest || './';
     dest = path.resolve(dest);
+
     console.log('backup ' + container + ' to ' + dest);
 
-    var containerName = container + '_image';
+    var containerConfig;
+    var containerName;
+    var imageName;
 
-    cmd.get(
-      'docker commit -p ' + container + ' ' + containerName,
-      function(data) {
-        cmd.get('docker save -o ' + dest + '/' + containerName + '.tar ' + containerName,
-          function(data) {
+    inspect(container)
+      .then(config => {
+        containerConfig = config;
+        containerName = String(containerConfig.Name).split('/').join('');
+        imageName = containerName + '_image';
 
-            cmd.get(
-              'docker inspect ' + container,
-              function(data) {
-                if (data) {
-                  data = JSON.parse(data)[0];
-                  if (data.Mounts && data.Mounts.length) {
+        cmd.run('echo "' + JSON.stringify(config, null, 2) + '" > ' + dest + '/' + containerName + '_config.json');
 
-                    var command = 'docker run --rm --volumes-from ' + container + ' -v ' + dest + ':/backup alpine';
-
-                    data.Mounts.forEach(function(volume) {
-                      if (volume.Name) {
-                        command += ' tar cvf /backup/' + container + '_volume_' + volume.Name + '.tar ' + volume.Destination;
-                      }
-                    });
-
-                    cmd.run(command);
-                    // cmd.run('docker logs docker-backup');
-                  }
-                }
-              }
-            );
-          }
-        );
-      }
-    );
+        commit(containerName, imageName)
+          .then(backupImage(imageName, dest))
+          .then(backupVolumes(containerName, containerConfig.Mounts, dest))
+          .then(backupRunCommand(containerName, dest));
+      });
   });
+
 
 program
   .command('restore <src> [container]')
-  .action(function(src, container) {
-
+  .action((src, container) => {
 
     if (src.indexOf('_image.tar') > 0) {
 
-      cmd.get(
-        'docker load < ' + src,
-        function(data) {
-          console.log(data);
-        });
+      cmd.get('docker load -i ' + src, console.log);
 
-    } else if (src.indexOf('_volume.tar') > 0) {
+    } else if (src.indexOf('_run.sh') > 0) {
+      cmd.get('bash ' + src, console.log);
+    } else if (src.indexOf('_volume_') > 0) {
 
       var dir = path.dirname(src);
+      dir = path.resolve(dir);
       var baseName = path.basename(src);
       var containerName = container || baseName.substr(0, baseName.indexOf('_'));
-      console.log('containerName', containerName);
-      console.log('docker run --rm --volumes-from ' + containerName + ' -v ' + dir + ':/backup some-ghost_image bash -c "cd / && tar xvf /backup/' + baseName + '"');
 
-      cmd.get(
-        'docker inspect ' + containerName,
-        function(data) {
-          if (data) {
-            data = JSON.parse(data)[0];
-            var image = data.Config.Image;
-
-            cmd.get(
-              'docker run --rm --volumes-from ' + containerName + ' -v ' + dir + ':/backup ' + image + ' bash -c "cd / && tar xvf /backup/' + baseName + '"',
-              function(data) {
-                console.log('data', data);
-              });
-
-          }
-        }
-      );
-
+      inspect(containerName)
+        .then(config => {
+          var image = config.Config.Image;
+          console.log(image);
+          cmd.get('docker run --rm --volumes-from ' + containerName + ' -v ' + dir + ':/backup '+ image +' bash -c "cd / && tar xvf /backup/' + baseName + '"', console.log);
+        });
     }
-
   });
 
 program.parse(process.argv);
